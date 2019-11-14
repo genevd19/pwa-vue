dcc0c73b212822043512136073104e07344ba2dd
stagiaire <stagiaire@cpg-453068.cepegra2008.lan>
2019-11-14 09:57:41 +0100

    ajout €

--- src/filters/Afficheprix.js
+++ src/filters/Afficheprix.js
@@ -1,6 +1,6 @@
-export default (value) => {
+export default (value,currency ='€') => {
     if( !value) return ''
         value = parseFloat(value)
         if(isNaN(value)) return ''
-        return value.toFixed(2) + " €"
+        return value.toFixed(2) + "" + currency 
 }
